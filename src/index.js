432ec01b96c03d479dc39427fc343d8e3d57c94e
ossama Johnson <houssam.arghal1997@gmail.com>
2019-12-31 20:50:07 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // IMPORTANT!!!! uncomment the lines below to make your tests work
-
+cont meInReact = createElement(
+  'div',
+  { className : 'me'},
+  [
+    createElement('h1', {}, 'a muslim person'),
+    createElement('p', {}, 'who is learning react'),
+    createElement(
+      'ul', 
+      { className: 'my-interests' },
+      [
+        createElement('li', {}, 'React'),
+        createElement('li', {}, 'JavaScript'),
+        createElement('li', {}, 'Series'),
+      ]
+    )
+  ]
+);
 ReactDOM.render(
   meInReact,
   document.getElementById('global')
