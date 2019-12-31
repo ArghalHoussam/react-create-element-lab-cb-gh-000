db7cea9a5f8f3cde64b57f7ccd7578c78f14aeec
ossama Johnson <houssam.arghal1997@gmail.com>
2019-12-31 20:51:31 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // IMPORTANT!!!! uncomment the lines below to make your tests work
-const meInReact = createElement(
+const meInReact = React.createElement(
   'div',
   { className : 'me'},
   [
